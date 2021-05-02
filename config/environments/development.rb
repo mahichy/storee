d877eeb1e24061bcf6dc5f74fb83f8aedb88e8ce
mahi <mohiddinoshim@yahoo.com>
2021-05-03 01:37:45 +0600

    smtp problem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
+
+    config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'oshimchy@gmail.com',
+    password: ENV["PASSWORD"],
+    authentication: 'plain',
+    # enable_starttls_auto: true
+    # props.put("mail.smtp.starttls.enable", "true");
+  }
 end
